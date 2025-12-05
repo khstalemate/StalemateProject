e386fab8121c6b3ea999b340efe3f4995b302580
GunWoo1281 <513gun@gmail.com>
2025-12-05 10:44:29 +0900

    Exclude SecurityAutoConfiguration from Spring Boot app
    Exclude SecurityAutoConfiguration from Spring Boot app

--- stalemateProject/src/main/java/com/stale/mate/StalemateProjectApplication.java
+++ stalemateProject/src/main/java/com/stale/mate/StalemateProjectApplication.java
@@ -3,7 +3,7 @@ package com.stale.mate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class StalemateProjectApplication {
 
 	public static void main(String[] args) {
