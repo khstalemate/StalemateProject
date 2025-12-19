ae2a3d34e51d05356931f367e4b5fb4bbe53ec6f
Y0-0N63 <boyun1625@naver.com>
2025-12-19 10:00:24 +0900

    20251219 최보윤 게시글 개수 조회

---
+++ stalemateProject/src/main/java/com/stale/mate/board/model/dto/Post.java
@@ -0,0 +1,22 @@
+package com.stale.mate.board.model.dto;
+
+public class Post {
+	private int postNo;
+	private String postTitle;
+	private String postDate;
+	private String category;
+	private String status;
+	private String postDelete;
+	private int views;
+	private int adoptFee;
+	private String missingDate;
+	private String missingTime;
+	private String location;
+	private String species;
+	private String gender;
+	private int age;
+	private int weight;
+	private String content;
+	private int menuNo;
+	private int memberNo;
+}
