61a1441cad0a8e1b2b0ca7b2fcf7605d82942f3e
seungjun393-pixel <seungjun393@gmail.com>
2025-12-24 17:33:10 +0900

    2025-12-24이승준 로그인관련

--- stalemateProject/src/main/java/com/stale/mate/main/controller/MainController.java
+++ stalemateProject/src/main/java/com/stale/mate/main/controller/MainController.java
@@ -3,6 +3,7 @@ package com.stale.mate.main.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -57,12 +58,13 @@ public class MainController {
 		return "common/mypage_report";
 	}
 
-	/*
-	 * //로그인 필터에서 로그인하지 않았을 때 리다이렉트로 요청
-	 * 
-	 * @GetMapping("loginError") public String errorPage(RedirectAttributes ra) {
-	 * ra.addFlashAttribute("message", "로그인 후 이용해주세요.");
-	 * 
-	 * return "redirect:/"; }
-	 */
+	
+	 //로그인 필터에서 로그인하지 않았을 때 리다이렉트로 요청 
+	 @GetMapping("loginError") 
+	 public String errorPage(RedirectAttributes ra) {
+		 ra.addFlashAttribute("message", "로그인 후 이용해주세요.");
+	  
+		 return "redirect:/"; 
+	}
+
 }
