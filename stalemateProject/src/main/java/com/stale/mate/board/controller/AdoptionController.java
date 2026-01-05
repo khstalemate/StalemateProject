d6d9a3a10814e7a87752aef67667e43e72f1c791
Y0-0N63 <boyun1625@naver.com>
2026-01-05 15:50:21 +0900

    adoption 적용

--- stalemateProject/src/main/java/com/stale/mate/board/controller/AdoptionController.java
+++ stalemateProject/src/main/java/com/stale/mate/board/controller/AdoptionController.java
@@ -1,18 +1,22 @@
 package com.stale.mate.board.controller;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.stale.mate.board.model.dto.Post;
@@ -170,5 +174,36 @@ public class AdoptionController {
 		}
 		
 		return "adoption/adoption_detail";
+	}
+	
+	/**
+	 * 작성자 : 최보윤
+	 * 작성일자 : 2026-01-05
+	 * '글쓰기' 버튼을 눌렀을 때 게시글 작성 페이지로 이동시키기 
+	 */
+	@GetMapping("insert")
+	public String showInsertPost() {
+		return "adoption/adoption_edit";
+	}
+	
+	public String insertPost(@ModelAttribute Post inputPost, @SessionAttribute("loginMember") Member loginMember,
+							@RequestParam("uploadImg") List<MultipartFile> images, RedirectAttributes ra) throws IllegalStateException, IOException{
+		inputPost.setMemberName(loginMember.getMemberNo());
+		int postNo = service.insertPost(inputPost, images);
+		
+		String path = null;
+		String message = null;
+		
+		if(postNo > 0) {
+			message = "게시글이 작성되었습니다.";
+			path = "/adoption/" + postNo;
+		} else {
+			message = "게시글 작성에 실패하였습니다.";
+			path = "/lostandfound/insert";
+		}
+		
+		ra.addFlashAttribute("message", message)
+		
+		return "redirect:" + path;
 	}
 }
